f328ccd4b3e093db01cc24d381d5266e6e34830a
YamadaKinji <109599270+YamadaKinji@users.noreply.github.com>
2024-02-28 04:34:47 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import re
 import asyncio
-from telegram import Bot
+# from telegram import Bot
 
 process_1 = subprocess.Popen(['python', 'APPLYDAILY.py'], stdout=subprocess.PIPE)
 output_1, _ = process_1.communicate()
@@ -41,17 +41,17 @@ merged_title = title1 + title2 + title3
 print(merged_title)
 print(merged_content)
 
-bot_token = os.environ.get('BOTTOKEN')
-chat_id = os.environ.get('USERID')
+# bot_token = os.environ.get('BOTTOKEN')
+# chat_id = os.environ.get('USERID')
 
 # 创建 Bot 实例
 
-bot = Bot(token=bot_token)
+# bot = Bot(token=bot_token)
 
 
 # 发送消息
-async def send_message():
-    await bot.send_message(chat_id=chat_id, text=merged_title + '\n' + merged_content)
+# async def send_message():
+#     await bot.send_message(chat_id=chat_id, text=merged_title + '\n' + merged_content)
 
 
 # 运行异步函数

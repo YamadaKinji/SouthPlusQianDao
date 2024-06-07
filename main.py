9fce58e789aaa3fe5416e532752bda1a44e72cc1
YamadaKinji <109599270+YamadaKinji@users.noreply.github.com>
2024-06-08 06:03:48 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,84 +1,104 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 import os
 import xml.etree.ElementTree as ET
 
 cookie_value = os.getenv('COOKIE')
+
 cookie_value = cookie_value.replace('\n', '').replace(' ', '')
 
 url = 'https://south-plus.net/plugin.php'
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
+common_headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-encoding': 'gzip, deflate, br, zstd',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'cookie': cookie_value,
+    'priority': 'u=0, i',
+    'sec-ch-ua': '"Microsoft Edge";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'
+}
 
-driver = webdriver.Chrome(options=chrome_options)
+a_headers = common_headers.copy()
+a_headers.update({
+    'referer': url+'?H_name-tasks-actions-newtasks.html.html'
+})
 
-def tasks(url, params, cookie_value, type):
-    try:
-        driver.get(url)
+c_headers = common_headers.copy()
+c_headers.update({
+    'authority': 'south-plus.net',
+    'method': 'GET',
+    'path': '/plugin.php?H_name-tasks-actions-newtasks.html.html',
+    'scheme': 'https',
+    'Referer': url+'?H_name-tasks.html.html'
+})
 
-        for cookie in cookie_value.split(';'):
-            name, value = cookie.split('=', 1)
-            driver.add_cookie({'name': name.strip(), 'value': value.strip()})
 
-        driver.get(f"{url}?{params}")
-        
-        # 等待并获取页面内容
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "body"))
-        )
-        data = driver.page_source
-        
-        # 打印响应内容的前500个字符以便调试
-        print(f"Response for {type}: {data[:500]}")
+common_params = {
+    'H_name': 'tasks',
+    'action': 'ajax',
+    'nowtime': '1717167492479',
+    'verify': '5af36471',
+}
 
-        # 保存原始响应内容到文件
-        with open(f'raw_response_{type}.html', 'w', encoding='utf-8') as f:
-            f.write(data)
+ad_params = common_params.copy()
+ad_params.update({
+    'actions': 'job',
+    'cid': '15',
+})
 
-        # 检查返回的数据是否包含HTML标签
-        if '<html' in data.lower():
-            raise Exception("服务器返回的内容包含HTML标签，可能是错误的请求或服务器问题。")
+aw_params = common_params.copy()
+aw_params.update({
+    'actions': 'job',
+    'cid': '14',
+})
 
-        # 解析XML数据
-        root = ET.fromstring(data)
-        cdata = root.text
+cd_params = common_params.copy()
+cd_params.update({
+    'actions': 'job2',
+    'cid': '15',
+})
 
-        # 提取变量值
-        values = cdata.split('\t')
-        if '申请' in type:
-            value_len = 2
-        else:
-            value_len = 3
-        if len(values) == value_len:
-            message = values[1]
-            print(type + message)
-        else:
-            raise Exception("XML格式不正确，请检查COOKIE设置")
-        if "还没超过" in message:
-            return False
-        else:
-            return True
+cw_params = common_params.copy()
+cw_params.update({
+    'actions': 'job2',
+    'cid': '14',
+})
 
-    except Exception as e:
-        print(f"任务执行错误: {e}")
+def tasks(url, params, headers, type):
+    response = requests.get(url, params=params, headers=headers)
+
+    response.encoding = 'utf-8'
+    data = response.text
+
+    # 解析XML数据
+    root = ET.fromstring(data)
+    cdata = root.text
+
+    # 提取变量值
+    values = cdata.split('\t')
+    if('申请' in type):
+        value_len = 2
+    else:
+        value_len = 3
+    if len(values) == value_len:
+        message = values[1]
+
+        print(type + message)
+    else:
+        raise Exception("XML格式不正确，请检查COOKIE设置")
+    if("还没超过" in message):
         return False
-    finally:
-        driver.quit()
-
-params = "H_name=tasks&action=ajax&nowtime=1717167492479&verify=5af36471&actions=job&cid=15"
-
-if tasks(url, params, cookie_value, "申请-日常: "):
-    params = "H_name=tasks&action=ajax&nowtime=1717167492479&verify=5af36471&actions=job2&cid=15"
-    tasks(url, params, cookie_value, "完成-日常: ")
-
-params = "H_name=tasks&action=ajax&nowtime=1717167492479&verify=5af36471&actions=job&cid=14"
-
-if tasks(url, params, cookie_value, "申请-周常: "):
-    params = "H_name=tasks&action=ajax&nowtime=1717167492479&verify=5af36471&actions=job2&cid=14"
-    tasks(url, params, cookie_value, "完成-周常: ")
+    else:
+        return True
+    
+if(tasks(url, ad_params, a_headers, "申请-日常: ")):
+    tasks(url, cd_params, c_headers, "完成-日常: ")
+if(tasks(url, aw_params, a_headers, "申请-周常: ")):
+    tasks(url, cw_params, c_headers, "完成-周常: ")
